46c9b68b9332baed769ff3e6efc784fde06820b5
princewritecode <princewritecode@gmail.com>
2025-04-12 10:09:22 +0530

    todo list version 0.2 added buttons and checkbox

--- script.js
+++ script.js
@@ -17,19 +17,24 @@ btn.addEventListener('click', e => {
         const createNewDiv = document.createElement('div');
         const deleteBtn = document.createElement('button');
         const createTaskPara = document.createElement('span');
-
         const checkBox = document.createElement('input'); // Create an input element
         checkBox.setAttribute('type', 'checkbox'); // Set its type to 'checkbox'
-
-
-
         deleteBtn.innerText = 'Delete';
-
         createTaskPara.textContent = inputValue;
         createNewDiv.append(createTaskPara);
         createNewDiv.append(deleteBtn);
-createTaskPara.append(checkBox);
+        createTaskPara.append(checkBox);
         document.body.append(createNewDiv);
+        checkBox.addEventListener('change', function (e) {
+            if (checkBox.checked) {
+                console.log('Checkbox is checked');
+                console.log(e.target.parentNode);
+                e.target.parentNode.style.textDecoration = 'line-through';
+            } else {
+                console.log('Checkbox is unchecked');
+                e.target.parentNode.style.textDecoration = 'none';
+            }
+        });
         deleteBtn.addEventListener('click', e => {
             e.target.parentNode.style.display='none';
             inputValue = '';
