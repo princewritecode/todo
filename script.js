05fabf8b372780cac3883d0d57b023f52cb38839
princewritecode <princewritecode@gmail.com>
2025-04-12 07:19:41 +0530

    todo list version 0.2 added buttons and checkbox

---
+++ script.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector('.add-btn');
+console.log(btn);
+const input = document.querySelector('.item-add-input');
+console.log(input)
+let inputValue;
+input.addEventListener('change', (e) => {
+  inputValue = e.target.value;
+})
+btn.addEventListener('click', e => {
+    // console.log('click');
+    // console.log(inputValue);
+
+    if (inputValue===undefined || inputValue === '') {
+        alert('List cannot be empty')
+    }
+    else{
+        const createNewDiv = document.createElement('div');
+        const deleteBtn = document.createElement('button');
+        const createTaskPara = document.createElement('span');
+
+        const checkBox = document.createElement('input'); // Create an input element
+        checkBox.setAttribute('type', 'checkbox'); // Set its type to 'checkbox'
+
+
+
+        deleteBtn.innerText = 'Delete';
+
+        createTaskPara.textContent = inputValue;
+        createNewDiv.append(createTaskPara);
+        createNewDiv.append(deleteBtn);
+createTaskPara.append(checkBox);
+        document.body.append(createNewDiv);
+        deleteBtn.addEventListener('click', e => {
+            e.target.parentNode.style.display='none';
+            inputValue = '';
+        })
+    }
+})
